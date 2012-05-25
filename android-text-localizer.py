b557271600c2272810c9518dedf61dbe71850d44
Joman Chu <code@notatypewriter.com>
2012-05-25 00:32:03 -0400

    Unify docstring and README

--- android-text-localizer.py
+++ android-text-localizer.py
@@ -23,6 +23,7 @@ duplicated in the strings.xml, a random string is appended.
 Errors and warnings are printed at the end of processing.
 
 Author: Joman Chu <github@notatypewriter.com>
+License: Public domain / Apache License 2.0 / GPLv3
 """
 
 import string
